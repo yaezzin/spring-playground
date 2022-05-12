3b342f400fda1a489ac4033609f8b65248d2fede
yejin <yaezzin@gmail.com>
2022-05-13 01:00:32 +0900

    @LoginUser로 개선하기

--- src/main/java/com/yaezzin/webservice/web/IndexController.java
+++ src/main/java/com/yaezzin/webservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.yaezzin.webservice.web;
 
+import com.yaezzin.webservice.config.auth.LoginUser;
 import com.yaezzin.webservice.config.auth.dto.SessionUser;
 import com.yaezzin.webservice.service.posts.PostsService;
 import com.yaezzin.webservice.web.dto.PostsResponseDto;
@@ -19,9 +20,11 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }

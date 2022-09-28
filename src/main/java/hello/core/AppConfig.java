cdfe96ab8a52f285cde70cd3c8fb7ef3c46d4438
yejin <yaezzin@gmail.com>
2022-09-28 22:27:52 +0900

    feat : 스프링으로 전환

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -6,12 +6,18 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(new MemoryMemberRepository());
     }
+
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
                 new MemoryMemberRepository(),
